991fd0c02f267615f97431805190b1737a70bc94
anirbanbasu33 <anirbanbasu33@gmail.com>
2025-07-28 08:21:06 +0530

    v3

--- Readme.txt
+++ Readme.txt
@@ -5,6 +5,8 @@ python3 -m venv <name of venv>
 view - command pallete - pythn:select interpreter - enter interpreter path --> ./venv/bin/python
 # Enabling venv for our command pallete-
 source venv/bin/activate 
+# Install necessary libraries
+pip install "fastapi[all]"
 
 
 ## STARTING THE FAST API PROGRAM
@@ -12,4 +14,12 @@ source venv/bin/activate
 uvicorn main:app --reload
 
 
-##
+## GIT commands
+git init
+git status
+git add main.py
+git status
+git commit -m "your commit name"
+git branch -M main
+git remote add origin https://github.com/anirbanbasu33/FASTAPI.git
+git push -u origin main
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def root():
@@ -12,6 +20,9 @@ def get_posts():
     return {"data": "Yours posts are here"}
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title: {payload["title"]} content: {payload["content"]}"} # Returned by API to postman
+def create_posts(post: Post):
+    print(post) # this is printing the pydantic model
+    # print(post.title)
+    print(post.model_dump()) # this is printing regular python dictionary
+    return {"data": post}
+    
