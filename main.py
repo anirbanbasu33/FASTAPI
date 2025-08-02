7acef902ede97f50f0d3cd5ae6561cee129ec874
anirbanbasu33 <anirbanbasu33@gmail.com>
2025-08-02 16:02:14 +0530

    v6 Delete & Update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -13,7 +13,7 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
-# Why I am hardcoding dummy posts-- so that everytime program restarts it has something to write on
+
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
             {"title": "favourite foods 1", "content": "I like pizza", "id": 2}]
 
@@ -22,31 +22,74 @@ def find_post(id):
     for i in my_posts:
         if i["id"] == id:
             return i
+        
+def find_index_post(id):
+    for index, element in enumerate(my_posts):
+        if element["id"] == id:
+            return index
             
-
+            
+## HOME
 @app.get("/")
 def root():
     return {"message": "Welcome to API Development 101"}
 
 
+## GETTING ALL POSTS
 @app.get("/posts")
 def get_posts():
     return {"data": my_posts}
 
 
-## Retrieving one individual post
-@app.get("/posts/{id}")  # id = path parameter...everytime we get a path parameter, even if its a number, it will be returned as a string...therefore we must manually convert it to an integer
-def get_post(id: int):   # Doing the above data validation
-    # print(type(id))
-    post = find_post(id)
-    print(post)
-    return {"post_detail": post}
-
-
-@app.post("/posts")
-def create_posts(post: Post): # Data validation with above Class
-    post_dict = post.model_dump() # to_dict()
-    post_dict['id'] = randrange(0,1000000) # assigning an id, normally DB does it, but here we are using random  
+## CREATING POSTS
+# Change the status code=201,inside decorator once something is created
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(post: Post): 
+    post_dict = post.model_dump() 
+    post_dict['id'] = randrange(0,1000000) 
     my_posts.append(post_dict)
     return {"data": post_dict}
+
+
+## GETTING THE LATEST POST
+@app.get("/posts/latest")
+def get_latest_post():
+    post = my_posts[len(my_posts)-1]
+    return {"detail": post}
+
+
+## GETTING AN INDIVIDUAL POST
+@app.get("/posts/{id}")  
+def get_post(id: int):   ## storing the response code I get in 'response' var
     
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with ID {id} does not exist")
+    return {"post_detail": post}
+
+    
+## DELETING A POST
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with ID: {id} does not exist")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+    
+
+## UPDATING A POST
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with ID: {id} does not exist")
+    
+    post_dict = post.model_dump() # Convert the data from frontend to a python dictionary
+    post_dict['id'] = id # Add the id into the dictionary
+    my_posts[index] = post_dict # Update that specific spot in the array
+    return {"data": post_dict}
